64a4ff86652566ca0ca3a300ee5fd4b5464b504f
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-02 01:42:23 +0100

    impr: remove data from design controller (close #4)

--- demo/aping-design-blanko.js
+++ demo/aping-design-blanko.js
@@ -7,18 +7,4 @@ angular.module('jtt_aping_design_blanko', [])
             $scope.workingCopy = $scope.results;
         });
 
-        $rootScope.array1 = [
-            {x: true, y: false},
-            {z: false, q: true},
-            {a: false, b: true}
-        ];
-
-        $rootScope.array2 = [
-            {c: true, d: false},
-            {e: false, h: true},
-            {f: false, g: true}
-        ];
-
-        $rootScope.object1 = {type:"simple object"};
-
     }]);
--- demo/app.js
+++ demo/app.js
@@ -1,1 +1,1 @@
-angular.module('app', ['jtt_aping', 'jtt_aping_ng_array', 'jtt_aping_design_blanko']);
+angular.module('app', ['jtt_aping', 'jtt_aping_ng_array', 'jtt_aping_design_blanko', 'data']);
